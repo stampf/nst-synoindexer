032cd92a345c984aa97d88bd5a72086e6d756a60
stampf <nicolas.stampf@gmail.com>
2014-11-11 16:23:38 +0100

    corrected bug, remove test and silent options as they caused too much problems

--- nst-synoindexer.sh
+++ nst-synoindexer.sh
@@ -6,7 +6,7 @@
 VIDEODIR="/volume1/video"
 
 # You shouldn't touch anything after this line
-TIMESTAMPFILE="/var/run/nst-synoindex.timestamp"
+TIMESTAMPFILE="/volume1/Scripts/nst-synoindex.timestamp"
 INDEXCMD="/usr/syno/bin/synoindex"
 
 ### Don't change absolutely anything below this line
@@ -18,7 +18,7 @@ fi
 
 usage() {
 	cat <<EOF
-Usage: $0 [-c] [-d dir] [-h] [-i index_cmd] [-n] [-r file] [-t] 
+Usage: $0 [-c] [-d dir] [-f n] [-h] [-i index_cmd] [-n] [-r file]
 	-c	: create reference file $TIMESTAMPFILE
 	-d dir	: use Video directory 'dir' instead of default $VIDEODIR
 	-f n	: force indexing of files since last 'n' days
@@ -26,19 +26,15 @@ Usage: $0 [-c] [-d dir] [-h] [-i index_cmd] [-n] [-r file] [-t]
 	-i index_cmd	: use 'index_cmd' instead of default $INDEXCMD (must accept -a <file> as argument)
 	-n	: do NOT change the timestamp of reference file $TIMESTAMPFILE
 	-r file	: set reference file to 'file' instead of default $TIMESTAMPFILE
-	-s	: silent mode (otherwise, indexed files are shown)
-	-t	: test only, echoes the commands, don't run them
 EOF
 	exit 0
 }
 
-TEST=0
 NOT=0
-VERBOSE="-print"
 DAYS="zzz"
 CREATE="no"
 
-while getopts "cd:f:hi:nr:st" opt; do
+while getopts "cd:f:hi:nr:" opt; do
 	case $opt in 
 	c) CREATE="yes";;
 	d) VIDEODIR=$OPTARG;;
@@ -47,18 +43,11 @@ while getopts "cd:f:hi:nr:st" opt; do
 	i) INDEXCMD=$OPTARG;;
 	n) NOT=1;;
 	r) TIMESTAMPFILE=$OPTARG;;
-	s) VERBOSE="";;
-	t) TEST=1;;
 	\?) usage;;
 	esac	
 done
 
-if [ $TEST = 1 ]; then
-	FINDEXEC=""
-	VERBOSE="-print"
-else
-	FINDEXEC="-exec $INDEXCMD -a {} \;"
-fi
+FINDEXEC="-exec $INDEXCMD -a {}"
 
 # check existence of synoindex
 if [ ! -x $INDEXCMD ]; then
@@ -68,7 +57,7 @@ fi
 
 # create reference file is asked for. If -t, then echo instead of doing it.
 if [ $CREATE = "yes" ]; then
-	$TEST touch $TIMESTAMPFILE
+	touch $TIMESTAMPFILE
 fi
 
 # if not forced days, then check existence of reference file
@@ -86,10 +75,11 @@ if [ ! -d $VIDEODIR ]; then
 fi
 
 # if not forced days, use reference file, otherwise use -mtime -$DAYS for find(1).
+
 if [ "$DAYS" = "zzz" ]; then
-	/usr/bin/find $VIDEODIR -type f -newer $TIMESTAMPFILE ! -regex '.*@eaDir.*' ! -name '*crdownload' $VERBOSE $FINDEXEC
+	/usr/bin/find $VIDEODIR -type f -newer $TIMESTAMPFILE ! -regex '.*@eaDir.*' ! -name '*crdownload' -print $FINDEXEC \;
 else
-	/usr/bin/find $VIDEODIR -type f -mtime -$DAYS ! -regex '.*@eaDir.*' ! -name '*crdownload' $VERBOSE $FINDEXEC
+	/usr/bin/find $VIDEODIR -type f -mtime -$DAYS ! -regex '.*@eaDir.*' ! -name '*crdownload' -print $FINDEXEC \;
 fi
 
 # if ask to NOT touch, then, well, do nothing (echo), instead (default): touch reference file.
